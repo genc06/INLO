b8ccc83802936ab6d050a77068756be420eba384
Genc Demi <139209233+genc06@users.noreply.github.com>
2023-12-20 13:59:04 +0100

    okay

--- password_generator.py
+++ password_generator.py
@@ -7,4 +7,11 @@ class PasswordGenerator:
 
     
     def generate_password(self):
-        return " "
+        # return " "
+        # return 'motdepasse'
+        # /refactor moment/
+        # return robusterator_de_mot_de_passe()
+        # return robusterator_de_mot_de_passe() 
+
+# def robusterator_de_mot_de_passe():
+    # return "motdepasse"
--- pwd_gen_tests.py
+++ pwd_gen_tests.py
@@ -4,7 +4,7 @@ from password_generator import PasswordGenerator
 
 class TestPwdGenerator(unittest.TestCase):
 
-    def test_basic_pwd_only_strings(self):
+    def test_basic_pwd_only_strings_red(self):
         gen_pwd = PasswordGenerator()
         self.assertEqual(gen_pwd.generate_password(),"motdepasse")
 
