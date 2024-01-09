3770a7d593e30d21d37782cf20b604e2e4f79ed8
Genc Demi <139209233+genc06@users.noreply.github.com>
2024-01-09 08:17:18 +0100

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -7,11 +7,11 @@ class PasswordGenerator:
 
     
     def generate_password(self):
-        # return " "
-        # return 'motdepasse'
-        # /refactor moment/
-        # return robusterator_de_mot_de_passe()
-        # return robusterator_de_mot_de_passe() 
-
+        #1 return " "
+        #2 return 'motdepasse'
+        #3 /refactor moment/
+        #4 return robusterator_de_mot_de_passe()
+        
 # def robusterator_de_mot_de_passe():
-    # return "motdepasse"
+        #mot_de_passe = "motdepasse"
+    # return mot_de_passe
