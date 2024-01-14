1b4fa183ec27ec069f5249aae9b8066375264ea8
Genc Demi <139209233+genc06@users.noreply.github.com>
2024-01-14 22:09:18 +0100

    corrigé des erreurs

--- password_generator.py
+++ password_generator.py
@@ -1,15 +1,17 @@
-
+# password_generator.py
 import random
 import string
 
 class PasswordGenerator:
-    def generate_password(self, length=12):
-        char = string.ascii_letters + string.digits + string.punctuation
-        pwd = ''.join(random.choice(char) for _ in range(length))
+    def __init__(self):
+        self.previous_password = None
 
-        
+    def generate_password(self, length=12):
+        chars = string.ascii_letters + string.digits + string.punctuation
+        pwd = ''.join(random.choice(chars) for _ in range(length))
+
         while pwd == self.previous_password:
-            pwd = ''.join(random.choice(char) for _ in range(length))
+            pwd = ''.join(random.choice(chars) for _ in range(length))
 
         self.previous_password = pwd
         return pwd
@@ -17,46 +19,26 @@ class PasswordGenerator:
     def generate_password_red(self):
         return " "
         
-        
     def generate_password_green(self):
         return 'motdepasse'
-    
     
     def robusterator_de_mot_de_passe_red(self):
         return 'motdepasse'
         
     def robusterator_de_mot_de_passe_green(self):
-        mdp_sans = "motdepasse"
-        # chiffre = "3"
-        # mdp_avec = mdp_sans + chiffre
-        # return mdp_sans + chiffre
         return 'motdepasse3'
     
     def mot_de_passe_plus_robuste_red(self):
-        mdp_sans = "motdepasse"
-        chiffre = "3"
-        mdp_avec = mdp_sans + chiffre
-        return mdp_avec
-    
+        return 'motdepasse3'
+
     def mot_de_passe_plus_robuste_green(self):
-        mdp_sans = "motdepasse"
-        chiffre = "3"
-        mdp_avec = mdp_sans.capitalize + chiffre
-        return mdp_avec
-    
+        return 'Motdepasse3'
+
     def mdp_encore_plus_robuste_red(self):
-        mdp_sans = "motdepasse"
-        chiffre = "3"
-        char = ""
-        #mdp_robuste = char + mdp_sans.capitalize() + chiffre 
-        return char + mdp_sans.capitalize() + chiffre 
-    
+        return '@motdepasse3'
+
     def mdp_encore_plus_robuste_green(self):
-        mdp_sans = "motdepasse"
-        chiffre = "3"
-        char = "@"
-        #mdp_robuste = char + mdp_sans.capitalize() + chiffre 
-        return char + mdp_sans.capitalize() + chiffre 
+        return '@Motdepasse3'
     
     def combinaison_pwd_complexe(self):
         lettres = string.ascii_letters
@@ -68,7 +50,6 @@ class PasswordGenerator:
         return complex_pwd
 
     def check_complexity(self, password):
-    
         return any(char.isdigit() for char in password) \
             and any(char.isupper() for char in password) \
             and any(char in "@#$%^&*()_+=-<>?!" for char in password)
